ac62bea6b27f94ec376aca0751b91562bade0ef7
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-12-19 08:53:22 +0900

    refactor : TODO 8. 테스트 코드 수정

--- src/test/java/com/example/demo/util/PasswordEncoderTest.java
+++ src/test/java/com/example/demo/util/PasswordEncoderTest.java
@@ -1,38 +1,41 @@
 package com.example.demo.util;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordEncoderTest {
-//    @Test
-//    void passwordEncodingTest() {
-//        String rawPassword ="asd123";
-//
-//        String encodedPassword = PasswordEncoder.encode(rawPassword);
-//
-//        assertNotNull(encodedPassword);
-//        System.out.println("Encoded Password : " + encodedPassword);
-//    }
-//
-//    @Test
-//    void isPasswordMatches() {
-//
-//        String rawPassword = "asd123";
-//        String encodedPassword = PasswordEncoder.encode(rawPassword);
-//        System.out.println("Encoded Password : " + encodedPassword);
-//
-//        boolean isMatch = PasswordEncoder.matches(rawPassword, encodedPassword);
-//
-//        assertTrue(isMatch, "패스워드 일치");
-//    }
-//    @Test
-//    void isPasswordNotMatches() {
-//
-//        String rawPassword = "asd123";
-//        String wrongPassword = "hello123";
-//        String encodedPassword = PasswordEncoder.encode(rawPassword);
-//        System.out.println("Encoded Password : " + encodedPassword);
-//
-//        boolean isMatch = PasswordEncoder.matches(wrongPassword, encodedPassword);
-//
-//        assertTrue(isMatch, "패스워드 불일치");
-//    }
+    @Test
+    void passwordEncodingTest() {
+        String rawPassword ="asd123";
+
+        String encodedPassword = PasswordEncoder.encode(rawPassword);
+
+        assertNotNull(encodedPassword);
+        System.out.println("Encoded Password : " + encodedPassword);
+    }
+
+    @Test
+    void isPasswordMatches() {
+
+        String rawPassword = "asd123";
+        String encodedPassword = PasswordEncoder.encode(rawPassword);
+        System.out.println("Encoded Password : " + encodedPassword);
+
+        boolean isMatch = PasswordEncoder.matches(rawPassword, encodedPassword);
+
+        assertTrue(isMatch, "패스워드 일치");
+    }
+    @Test
+    void isPasswordNotMatches() {
+
+        String rawPassword = "asd123";
+        String wrongPassword = "hello123";
+        String encodedPassword = PasswordEncoder.encode(rawPassword);
+        System.out.println("Encoded Password : " + encodedPassword);
+
+        boolean isMatch = PasswordEncoder.matches(wrongPassword, encodedPassword);
+
+        assertTrue(isMatch, "패스워드 불일치");
+    }
 
 }
