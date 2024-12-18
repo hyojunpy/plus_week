5b02aecc58307aa0da93fdbee45bd11e43e36289
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-12-18 12:38:02 +0900

    feat : TODO 2. 인가에 대한 이해

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import com.example.demo.entity.Role;
 import com.example.demo.filter.AuthFilter;
 import com.example.demo.filter.RoleFilter;
+import com.example.demo.interceptor.AdminInterceptor;
 import com.example.demo.interceptor.AuthInterceptor;
 import com.example.demo.interceptor.UserRoleInterceptor;
 import jakarta.servlet.Filter;
@@ -11,19 +12,22 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer, HandlerInterceptor {
 
     // TODO: 2. 인가에 대한 이해
-    private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/admins/*", "/items/*"};
+    private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/items/*"};
     private static final String[] USER_ROLE_REQUIRED_PATH_PATTERNS = {"/reservations/*"};
+    private static final String[] ADMIN_REQUIRED_PATH_PATTERNS = {"/admins/*"};
 
     private final AuthInterceptor authInterceptor;
     private final UserRoleInterceptor userRoleInterceptor;
+    private final AdminInterceptor adminInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -34,6 +38,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(userRoleInterceptor)
                 .addPathPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE + 2);
+
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns(ADMIN_REQUIRED_PATH_PATTERNS)
+                .order(Ordered.HIGHEST_PRECEDENCE + 1);
     }
 
     @Bean
@@ -51,6 +59,15 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setFilter(new RoleFilter(Role.USER));
         filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 2);
         filterRegistrationBean.addUrlPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean adminFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new RoleFilter(Role.USER));
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        filterRegistrationBean.addUrlPatterns(ADMIN_REQUIRED_PATH_PATTERNS);
         return filterRegistrationBean;
     }
 }
